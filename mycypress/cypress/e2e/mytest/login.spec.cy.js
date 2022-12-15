aef03fb133994b9dad4f3d1acf683fe3882588b9
Helio Franca <heliordf@hotmail.com>
2022-12-15 00:31:18 -0300

    screenshot definido por pasta

--- mycypress/cypress/e2e/mytest/login.spec.cy.js
+++ mycypress/cypress/e2e/mytest/login.spec.cy.js
@@ -10,13 +10,14 @@ context('Funcionalidade Login', () => {
     });
 
     afterEach(() => {
-        cy.screenshot()
+        //Após cada CT
     });
     it('Deve fazer login com sucesso', () => {
         cy.get('#username').type('heliordf@hotmail.com')
         cy.get('#password').type('killer17')
         cy.get('.woocommerce-form > .button').click()
         cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá,')
+        cy.screenshot("login/Deve fazer login com sucesso")
     })
 
     it('Deve exibir uma mensagem de erro ao inserir uma senha inválida', () => {
@@ -24,6 +25,7 @@ context('Funcionalidade Login', () => {
         cy.get('#password').type('killer177')
         cy.get('.woocommerce-form > .button').click()
         cy.get('.woocommerce-error').should('contain', 'a senha fornecida para o e-mail heliordf@hotmail.com está incorreta. Perdeu a senha?')
+        cy.screenshot("login/Deve exibir uma mensagem de erro ao inserir uma senha inválida")
     })
 
     it('Deve exibir uma mensagem de erro ao inserir um usuário inválido', () => {
@@ -31,6 +33,7 @@ context('Funcionalidade Login', () => {
         cy.get('#password').type('killer17')
         cy.get('.woocommerce-form > .button').click()
         cy.get('.woocommerce-error').should('contain', 'Endereço de e-mail desconhecido. Verifique novamente ou tente seu nome de usuário.')
+        cy.screenshot("login/Deve exibir uma mensagem de erro ao inserir um usuário inválido")
     })
 })
 
