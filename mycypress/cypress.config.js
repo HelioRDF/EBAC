b1e3665ee035e9e68c9692ff4d6429c0a9e31d99
Helio Franca <heliordf@hotmail.com>
2023-01-04 16:14:32 -0300

    Update

--- mycypress/cypress.config.js
+++ mycypress/cypress.config.js
@@ -4,12 +4,13 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    "baseUrl":"http://lojaebac.ebaconline.art.br/",
+    "video": false,
+    "baseUrl": "http://lojaebac.ebaconline.art.br/",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
   },
- 
-   
+
+
 });
 
